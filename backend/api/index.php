c71185a86fb3b92bb435d561c81dd6abb2945d82
curtis <kinegenicurtis@gmail.com>
2026-09-09 15:40:48 +0300

    Prepare Stockroom for Vercel deployment

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,3 +1,18 @@
 <?php
 
+$aivenCa = getenv('AIVEN_MYSQL_CA');
+
+if (is_string($aivenCa) && $aivenCa !== '') {
+    $caPath = '/tmp/aiven-mysql-ca.pem';
+
+    if (file_put_contents($caPath, $aivenCa) === false) {
+        http_response_code(500);
+        exit('Unable to prepare the database TLS certificate.');
+    }
+
+    putenv('MYSQL_ATTR_SSL_CA='.$caPath);
+    $_ENV['MYSQL_ATTR_SSL_CA'] = $caPath;
+    $_SERVER['MYSQL_ATTR_SSL_CA'] = $caPath;
+}
+
 require __DIR__.'/../public/index.php';
