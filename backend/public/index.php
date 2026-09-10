538a0f6021b3860b2f0ca2a759941a81ac9acefe
curtis <kinegenicurtis@gmail.com>
2026-09-10 10:51:28 +0300

    Prepare Stockroom for InfinityFree deployment

--- backend/public/index.php
+++ backend/public/index.php
@@ -11,10 +11,15 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+$applicationPath = is_dir(__DIR__.'/../backend')
+    ? __DIR__.'/../backend'
+    : dirname(__DIR__);
+
+require $applicationPath.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once $applicationPath.'/bootstrap/app.php';
+$app->usePublicPath(__DIR__);
 
 $app->handleRequest(Request::capture());
